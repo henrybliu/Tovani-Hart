852fff6563bfc72c6c0ded92aa09991069bc5650
Henry Liu <87534839+henrybliu@users.noreply.github.com>
2025-02-20 09:40:36 -0800

    Update Jane's name

--- src/sections/team/TEAM_TEXT.tsx
+++ src/sections/team/TEAM_TEXT.tsx
@@ -7,7 +7,7 @@ export type EmployeeProps = {
 
 const TEAM_TEXT: EmployeeProps[] = [
   {
-    name: "Jane Locke",
+    name: "Jane Locke testing",
     title: "Project Coordinator",
     biography:
       "Jane has over 20 years of experience ranging from hands on civic service with Habitat for Humanity to coordinating consultants and contractor efforts through complex construction projects. She brings enthusiasm to each project placing highest priority on fulfilling our client's needs. She operates efficiently in all phases of work. Her immersion into projects that range from programming to construction administration has contributed to her well -rounded approach to problem solving. Jane is instrumental in the organization, assembly and distribution of design documents from initial educational planning meetings, jurisdictional applications through construction close out. Jane is a welcoming presence to people and is a dedicated member of Tovani Hart and is a critical team member.",
