b276f67282aa3e59346e65a9426285a7761d12ec
Henry Liu <87534839+henrybliu@users.noreply.github.com>
2025-02-20 08:20:27 -0800

    Update Home Subheader

--- src/sections/home/HomeSection.tsx
+++ src/sections/home/HomeSection.tsx
@@ -3,7 +3,7 @@ import SectionHeader from "../../components/SectionHeader";
 const HomeSection = () => {
   return (
     <div>
-      <SectionHeader>Tovani Hart is now hiring</SectionHeader>
+      <SectionHeader>Tovani Hart is now hiring testing</SectionHeader>
     </div>
   );
 };
