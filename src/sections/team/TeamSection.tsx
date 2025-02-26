26784be72c5b4fc0e2d6b82c13cbe0d6318f3e64
Henry Liu <11.henrybliu@gmail.com>
2025-02-26 16:13:17 -0700

    fix TeamContent variables to test build

--- src/sections/team/TeamSection.tsx
+++ src/sections/team/TeamSection.tsx
@@ -2,7 +2,7 @@ import SectionHeader from "../../components/SectionHeader";
 import TEAM_CONSTANTS from "./TEAM_CONSTANTS";
 import Section from "../../components/Section";
 import SectionContent from "../../components/SectionContent";
-import { styled } from "@mui/material";
+// import { styled } from "@mui/material";
 import ParallaxImage from "../../components/ParallaxImage";
 import useMobileScreen from "../../utils/useMobileScreen";
 
@@ -39,8 +39,8 @@ const TeamContent = () => {
   );
 };
 
-const Container = styled("div")({
-  height: "20px",
-});
+// const Container = styled("div")({
+//   height: "20px",
+// });
 
 export default TeamSection;
